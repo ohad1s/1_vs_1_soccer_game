426d54d61ca164f6d06d95291a07ebc2461fe8fd
Ohad Shirazi <92723105+ohad1s@users.noreply.github.com>
2022-08-11 17:56:47 +0300

    docu

--- soccer_game.py
+++ soccer_game.py
@@ -15,6 +15,7 @@ FPS = 60
 PLAYER_WIDTH, PLAYER_HEIGHT = 155, 140
 VEL = 5
 
+
 class Game:
 
     def __init__(self) -> None:
@@ -62,9 +63,12 @@ class Game:
         self.START_TIME = 90
         self.FRAME_RATE = 60
 
-        self.is_goal=False
+        self.is_goal = False
 
-    def draw_window(self, hal, vini, hal_goals, vini_goals, ball, goal_right, goal_left,target_r,target_l):
+    def draw_window(self, hal, vini, hal_goals, vini_goals, ball, goal_right, goal_left, target_r, target_l):
+        '''
+            A method responsible for organizing the screen display
+        '''
         self.screen.blit(self.bg, (0, 0))
 
         hal_text = self.GOALS_FONT.render("Goals: " + str(hal_goals), 1, BLACK)
@@ -75,7 +79,8 @@ class Game:
         self.screen.blit(self.hal, (hal.x, hal.y))
         self.screen.blit(self.vini, (vini.x, vini.y))
         self.screen.blit(self.ball, (ball.x, ball.y))
-        #for debug goals:
+
+        # for debug goals:
         # pygame.draw.rect(self.screen,color=RED,rect=goal_right)
         # pygame.draw.rect(self.screen,color=RED,rect=goal_left)
         # pygame.draw.rect(self.screen,color=GREEN,rect=target_r[0])
@@ -85,21 +90,27 @@ class Game:
         # pygame.draw.rect(self.screen,color=GREEN,rect=target_r[2])
         # pygame.draw.rect(self.screen,color=GREEN,rect=target_l[2])
 
-        total_sec=self.START_TIME-(self.FRAME_COUNT//self.FRAME_RATE)
-        if total_sec<0:
-            total_sec=0
-        self.min=total_sec//60
-        self.sec=total_sec%60
-        output_time="Time left: {0:02}:{1:02}".format(self.min,self.sec)
+        total_sec = self.START_TIME - (self.FRAME_COUNT // self.FRAME_RATE)
+        if total_sec < 0:
+            total_sec = 0
+        self.min = total_sec // 60
+        self.sec = total_sec % 60
+        output_time = "Time left: {0:02}:{1:02}".format(self.min, self.sec)
         clock_text = self.GOALS_FONT.render(output_time, 1, BLACK)
-        self.screen.blit(clock_text, (self.width/2 -250, 10))
+        self.screen.blit(clock_text, (self.width / 2 - 250, 10))
         pygame.display.update()
 
-    def restart_ball(self,ball):
-        ball.y=(self.width / 2) - 40
-        ball.x=(self.height / 2) - 60
+    def restart_ball(self, ball):
+        '''
+            A method to restart ball position after a goal
+        '''
+        ball.y = (self.width / 2) - 40
+        ball.x = (self.height / 2) - 60
 
     def celebrate_goal(self):
+        '''
+            A method responsible for celebrating a goal
+        '''
         self.GOAL_SOUND.play()
         cii_text = self.GOALS_FONT.render("Ciiiiii!!!", 1, BLACK)
         self.screen.blit(cii_text, (self.width / 2 - 200, 100))
@@ -116,23 +127,30 @@ class Game:
         self.screen.blit(self.ciir7, (self.width / 2 - 350, 200))
         pygame.display.update()
 
-    def ball_moving(self, ball, vini, hal,goal_left, goal_right,target_r,target_l):
+    def ball_moving(self, ball, vini, hal, goal_left, goal_right, target_r, target_l):
+        '''
+            A method responsible for moving the ball
+            in cases:
+            - ball touching the screen frame / ball hit the post
+            - player touches the ball
+            - goal
+        '''
         ball.y += self.BALL_SPEED_Y
         ball.x += self.BALL_SPEED_X
         COLL_TOLL = 10
-        COLL_TOLL_GOAL= 7
+        COLL_TOLL_GOAL = 7
 
         if ball.colliderect(goal_left):
             if abs(goal_left.right - ball.left) < COLL_TOLL_GOAL:
-                self.hal_goals+=1
+                self.hal_goals += 1
                 self.restart_ball(ball)
-                self.is_goal=True
+                self.is_goal = True
 
         if ball.colliderect(goal_right):
             if abs(goal_right.left - ball.right) < COLL_TOLL_GOAL:
-                self.vini_goals+=1
+                self.vini_goals += 1
                 self.restart_ball(ball)
-                self.is_goal=True
+                self.is_goal = True
 
         if ball.colliderect(target_r[0]):
             if abs(target_r[0].top - ball.bottom) < COLL_TOLL and self.BALL_SPEED_Y > 0:
@@ -194,9 +212,9 @@ class Game:
             if abs(target_l[2].left - ball.right) < COLL_TOLL and self.BALL_SPEED_X > 0:
                 self.BALL_SPEED_X *= -1
 
-        if ball.right >= self.width -40 or ball.left <= 0:
+        if ball.right >= self.width - 40 or ball.left <= 0:
             self.BALL_SPEED_X *= -1
-        if ball.bottom >= self.height -80 or ball.top <= 0:
+        if ball.bottom >= self.height - 80 or ball.top <= 0:
             self.BALL_SPEED_Y *= -1
         if ball.colliderect(vini):
             self.KICK_SOUND.play()
@@ -220,6 +238,9 @@ class Game:
                 self.BALL_SPEED_X *= -1
 
     def vini_handle_movment(self, key_pressed, vini):
+        '''
+            A method responsible for moving player one
+        '''
         if key_pressed[pygame.K_a] and vini.x - VEL > 0:  # left
             vini.x -= VEL
         if key_pressed[pygame.K_d] and vini.x + VEL + vini.width - 1 < self.BORDER.x + 22:  # right
@@ -230,6 +251,9 @@ class Game:
             vini.y += VEL
 
     def hal_handle_movment(self, key_pressed, hal):
+        '''
+            A method responsible for moving player two
+        '''
         if key_pressed[pygame.K_LEFT] and hal.x - VEL > self.BORDER.x - 18:  # left
             hal.x -= VEL
         if key_pressed[pygame.K_RIGHT] and hal.x + VEL + hal.width - 10 < self.width:  # right
@@ -239,44 +263,51 @@ class Game:
         if key_pressed[pygame.K_DOWN] and hal.y + VEL + hal.width < self.height:  # down
             hal.y += VEL
 
-    def draw_winner(self,text):
+    def draw_winner(self, text):
+        '''
+            A method responsible for the announcement of the winner
+        '''
         WINNER_FONT = pygame.font.SysFont('comicsans', 100)
         draw_text = WINNER_FONT.render(text, 1, WHITE)
-        if text=="Tie":
-            self.screen.blit(draw_text, (self.width / 2 - draw_text.get_width() / 2, self.height / 2 - draw_text.get_height() / 2))
-        if text=="Halland":
-            self.screen.blit(draw_text, (self.width/2 -200, 100))
+        if text == "Tie":
+            self.screen.blit(draw_text,
+                             (self.width / 2 - draw_text.get_width() / 2, self.height / 2 - draw_text.get_height() / 2))
+        if text == "Halland":
+            self.screen.blit(draw_text, (self.width / 2 - 200, 100))
             Gold_Ball = pygame.image.load('pics&aud/hal_win.png')
             self.gold = pygame.transform.rotate(pygame.transform.scale(
-                Gold_Ball, (PLAYER_WIDTH*4, PLAYER_HEIGHT*4)), 0)
-            self.screen.blit(self.gold,(self.width/2 -200, 200))
-        if text=="Rodrygo":
-            self.screen.blit(draw_text, (self.width/2 -350, 100))
+                Gold_Ball, (PLAYER_WIDTH * 4, PLAYER_HEIGHT * 4)), 0)
+            self.screen.blit(self.gold, (self.width / 2 - 200, 200))
+        if text == "Rodrygo":
+            self.screen.blit(draw_text, (self.width / 2 - 350, 100))
             Gold_Ball = pygame.image.load('pics&aud/vin_win.png')
             self.gold = pygame.transform.rotate(pygame.transform.scale(
-                Gold_Ball, (PLAYER_WIDTH*4, PLAYER_HEIGHT*4)), 0)
-            self.screen.blit(self.gold,(self.width/2 -200, 200))
+                Gold_Ball, (PLAYER_WIDTH * 4, PLAYER_HEIGHT * 4)), 0)
+            self.screen.blit(self.gold, (self.width / 2 - 200, 200))
         pygame.display.update()
         pygame.time.delay(5000)
 
     def play(self):
+        '''
+            main loop in order to play the game
+        '''
         hal = pygame.Rect(1700, 390, PLAYER_WIDTH, PLAYER_HEIGHT)
         vini = pygame.Rect(60, 390, PLAYER_WIDTH, PLAYER_HEIGHT)
         ball = pygame.Rect((self.width / 2) - 40, (self.height / 2) - 60, PLAYER_WIDTH / 4, PLAYER_HEIGHT / 4)
-        goal_right = pygame.Rect(1800,435, 5, PLAYER_HEIGHT / 1.25)
+        goal_right = pygame.Rect(1800, 435, 5, PLAYER_HEIGHT / 1.25)
         goal_left = pygame.Rect(80, 435, 5, PLAYER_HEIGHT / 1.25)
-        back_right = pygame.Rect(1840,425, 5, PLAYER_HEIGHT / 1.05)
+        back_right = pygame.Rect(1840, 425, 5, PLAYER_HEIGHT / 1.05)
         back_left = pygame.Rect(40, 425, 5, PLAYER_HEIGHT / 1.05)
-        up_right = pygame.Rect(1770,425, PLAYER_WIDTH/2, 5)
-        up_left = pygame.Rect(40, 425, PLAYER_WIDTH/2, 5)
-        down_right = pygame.Rect(1770,550, PLAYER_WIDTH/2, 5)
-        down_left = pygame.Rect(40, 550, PLAYER_WIDTH/2, 5)
-        target_r=[back_right, up_right,down_right]
-        target_l=[back_left, up_left,down_left]
+        up_right = pygame.Rect(1770, 425, PLAYER_WIDTH / 2, 5)
+        up_left = pygame.Rect(40, 425, PLAYER_WIDTH / 2, 5)
+        down_right = pygame.Rect(1770, 550, PLAYER_WIDTH / 2, 5)
+        down_left = pygame.Rect(40, 550, PLAYER_WIDTH / 2, 5)
+        target_r = [back_right, up_right, down_right]
+        target_l = [back_left, up_left, down_left]
         self.clock = pygame.time.Clock()
         run = True
         while run:
-            self.FRAME_COUNT+=1
+            self.FRAME_COUNT += 1
             self.clock.tick(FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -287,25 +318,27 @@ class Game:
             key_pressed = pygame.key.get_pressed()
             self.vini_handle_movment(key_pressed, vini)
             self.hal_handle_movment(key_pressed, hal)
-            self.ball_moving(ball, vini, hal,goal_left,goal_right,target_r,target_l)
-            self.draw_window(hal, vini, self.hal_goals, self.vini_goals, ball,goal_right,goal_left,target_r,target_l)
-            if self.is_goal==True:
+            self.ball_moving(ball, vini, hal, goal_left, goal_right, target_r, target_l)
+            self.draw_window(hal, vini, self.hal_goals, self.vini_goals, ball, goal_right, goal_left, target_r,
+                             target_l)
+            if self.is_goal == True:
                 self.celebrate_goal()
                 time.wait(2000)
-                self.is_goal=False
+                self.is_goal = False
 
-            if self.min==0 and self.sec==0:
-                winner_text=""
-                if self.hal_goals>self.vini_goals:
-                    winner_text="Halland"
-                if self.vini_goals>self.hal_goals:
-                    winner_text="Rodrygo"
-                if self.hal_goals==self.vini_goals:
-                    winner_text="Tie"
+            if self.min == 0 and self.sec == 0:
+                winner_text = ""
+                if self.hal_goals > self.vini_goals:
+                    winner_text = "Halland"
+                if self.vini_goals > self.hal_goals:
+                    winner_text = "Rodrygo"
+                if self.hal_goals == self.vini_goals:
+                    winner_text = "Tie"
                 self.draw_winner(winner_text)
                 break
 
         pygame.quit()
+
 
 x = Game()
 x.play()
